07d9688ff774a557b54021f5a0548da417af4f7b
PakL <webmaster@paklweb.de>
2020-08-28 13:01:19 +0200

    Broadcast when OverlayHost-Page is ready so other addons can add their settings

--- addons/overlayhost/renderer.js
+++ addons/overlayhost/renderer.js
@@ -1,3 +1,5 @@
+const Broadcast = require('../../dist/dev.pakl.ttvst/renderer/Broadcast');
 const OverlayPage = require('./OverlayPage');
 
 TTVST.ui.addPage(new OverlayPage());
+Broadcast.instance.emit('app.ttvst.overlay.ready');
