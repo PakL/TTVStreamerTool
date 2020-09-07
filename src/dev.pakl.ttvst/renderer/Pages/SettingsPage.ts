ae422d6ffe5c83c0f87b7a15defd068c62b97940
PakL <webmaster@paklweb.de>
2020-09-07 21:02:04 +0200

    Allow for settings to be added

--- src/dev.pakl.ttvst/renderer/Pages/SettingsPage.ts
+++ src/dev.pakl.ttvst/renderer/Pages/SettingsPage.ts
@@ -5,15 +5,19 @@ import * as riot from 'riot';
 
 import SettingsMenu from '../../../../dist/dev.pakl.ttvst/renderer/UI/Settings/SettingsMenu';
 import SettingsConfig from '../UI/Settings/SettingsConfiguration';
+import { ISettingsSetProps } from '../UI/Settings/SettingsConfiguration';
 
 declare var TTVST: _ttvst;
 
 class SettingsPage extends Page {
 
-	settings: riot.RiotComponent = null;
+	settingsCmpnt: riot.RiotComponent = null;
+	settings: Array<ISettingsSetProps> = [];
 
 	constructor() {
 		super('Settings');
+
+		this.settings = this.settings.concat(SettingsConfig);
 	}
 
 	get icon(): any {
@@ -22,11 +26,20 @@ class SettingsPage extends Page {
 
 	content(): HTMLElement {
 		let settCmpnt = riot.component<null, null>(SettingsMenu);
-		this.settings = settCmpnt(document.createElement('SettingsMenu'));
+		this.settingsCmpnt = settCmpnt(document.createElement('SettingsMenu'));
 
-		this.settings.setSettings(SettingsConfig);
+		this.settingsCmpnt.setSettings(this.settings);
 
-		return this.settings.root;
+		return this.settingsCmpnt.root;
+	}
+
+	addSettingsSet(settings: ISettingsSetProps) {
+		this.settings.push(settings);
+		this.settingsCmpnt.setSettings(this.settings);
+	}
+
+	update() {
+		this.settingsCmpnt.update();
 	}
 
 }
