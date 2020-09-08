5006057c7c46cf55cd64152fcc9473d2eac8d8b5
PakL <webmaster@paklweb.de>
2020-09-08 18:46:28 +0200

    *cough*

--- addons/overlayhost/renderer.js
+++ addons/overlayhost/renderer.js
@@ -1,5 +1,4 @@
-const Broadcast = require('../../dist/dev.pakl.ttvst/renderer/Broadcast');
 const OverlayPage = require('./OverlayPage');
 
 TTVST.ui.addPage(new OverlayPage());
-Broadcast.instance.emit('app.ttvst.overlay.ready');
+TTVST.Broadcast.instance.emit('app.ttvst.overlay.ready');
