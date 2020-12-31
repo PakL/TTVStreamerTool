bd647bbfe2f81c5b8abe4d5f85c88380d9d672d4
PakL <webmaster@paklweb.de>
2020-12-31 14:03:04 +0100

    Optionally use Settings with sessionStorage

--- src/dev.pakl.ttvst/main/Util/Settings.ts
+++ src/dev.pakl.ttvst/main/Util/Settings.ts
@@ -12,68 +12,68 @@ const createRequestId = () => {
 	return hrtime[0].toString(16) + '-' + hrtime[1].toString(16);
 }
 
-export function getBoolean(name: string, defaultValue: boolean): Promise<boolean> {
+export function getBoolean(name: string, defaultValue: boolean, session: boolean = false): Promise<boolean> {
 	if(typeof(TTVST) === 'undefined' || TTVST.mainWindow === null || TTVST.mainWindow.window === null) return Promise.resolve(defaultValue);
 	return new Promise((resolve) => {
 		let rid = createRequestId();
 		ipcMain.once(`Settings.gotBoolean.${name}.${rid}`, (event, value: boolean) => {
 			resolve(value);
 		});
-		TTVST.mainWindow.ipcSend('Settings.getBoolean', rid, name, defaultValue);
+		TTVST.mainWindow.ipcSend('Settings.getBoolean', rid, name, defaultValue, session);
 	});
 }
 
-export function setBoolean(name: string, value: boolean): Promise<boolean> {
+export function setBoolean(name: string, value: boolean, session: boolean = false): Promise<boolean> {
 	if(typeof(TTVST) === 'undefined' || TTVST.mainWindow === null || TTVST.mainWindow.window === null) return Promise.resolve(value);
 	return new Promise((resolve) => {
 		let rid = createRequestId();
 		ipcMain.once(`Settings.sotBoolean.${name}.${rid}`, (event, value: boolean) => {
 			resolve(value);
 		});
-		TTVST.mainWindow.ipcSend('Settings.setBoolean', rid, name, value);
+		TTVST.mainWindow.ipcSend('Settings.setBoolean', rid, name, value, session);
 	});
 }
 
-export function getString(name: string, defaultValue: string): Promise<string> {
+export function getString(name: string, defaultValue: string, session: boolean = false): Promise<string> {
 	if(typeof(TTVST) === 'undefined' || TTVST.mainWindow === null || TTVST.mainWindow.window === null) return Promise.resolve(defaultValue);
 	return new Promise((resolve) => {
 		let rid = createRequestId();
 		ipcMain.once(`Settings.gotString.${name}.${rid}`, (event, value: string) => {
 			resolve(value);
 		});
-		TTVST.mainWindow.ipcSend('Settings.getString', rid, name, defaultValue);
+		TTVST.mainWindow.ipcSend('Settings.getString', rid, name, defaultValue, session);
 	});
 }
 
-export function setString(name: string, value: string): Promise<string> {
+export function setString(name: string, value: string, session: boolean = false): Promise<string> {
 	if(typeof(TTVST) === 'undefined' || TTVST.mainWindow === null || TTVST.mainWindow.window === null) return Promise.resolve(value);
 	return new Promise((resolve) => {
 		let rid = createRequestId();
 		ipcMain.once(`Settings.sotString.${name}.${rid}`, (event, value: string) => {
 			resolve(value);
 		});
-		TTVST.mainWindow.ipcSend('Settings.setString', rid, name, value);
+		TTVST.mainWindow.ipcSend('Settings.setString', rid, name, value, session);
 	});
 }
 
-export function getJSON(name: string, defaultValue: any): Promise<any> {
+export function getJSON(name: string, defaultValue: any, session: boolean = false): Promise<any> {
 	if(typeof(TTVST) === 'undefined' || TTVST.mainWindow === null || TTVST.mainWindow.window === null) return Promise.resolve(defaultValue);
 	return new Promise((resolve) => {
 		let rid = createRequestId();
 		ipcMain.once(`Settings.gotJSON.${name}.${rid}`, (event, value: any) => {
 			resolve(value);
 		});
-		TTVST.mainWindow.ipcSend('Settings.getJSON', rid, name, defaultValue);
+		TTVST.mainWindow.ipcSend('Settings.getJSON', rid, name, defaultValue, session);
 	});
 }
 
-export function setJSON(name: string, value: any): Promise<any> {
+export function setJSON(name: string, value: any, session: boolean = false): Promise<any> {
 	if(typeof(TTVST) === 'undefined' || TTVST.mainWindow === null || TTVST.mainWindow.window === null) return Promise.resolve(value);
 	return new Promise((resolve) => {
 		let rid = createRequestId();
 		ipcMain.once(`Settings.sotJSON.${name}.${rid}`, (event, value: any) => {
 			resolve(value);
 		});
-		TTVST.mainWindow.ipcSend('Settings.setJSON', rid, name, value);
+		TTVST.mainWindow.ipcSend('Settings.setJSON', rid, name, value, session);
 	});
 }
--- src/dev.pakl.ttvst/renderer/Settings.ts
+++ src/dev.pakl.ttvst/renderer/Settings.ts
@@ -5,23 +5,23 @@ const _storageRemoveHolder = window.localStorage.removeItem
 
 const availableLanguages = ['en', 'de']
 
-ipcRenderer.on('Settings.getBoolean', (event, rid: string, name: string, defaultValue: boolean) => {
-	ipcRenderer.send(`Settings.gotBoolean.${name}.${rid}`, getBoolean(name, defaultValue));
+ipcRenderer.on('Settings.getBoolean', (event, rid: string, name: string, defaultValue: boolean, session: boolean = false) => {
+	ipcRenderer.send(`Settings.gotBoolean.${name}.${rid}`, getBoolean(name, defaultValue, session));
 });
-ipcRenderer.on('Settings.setBoolean', (event, rid: string, name: string, value: boolean) => {
-	ipcRenderer.send(`Settings.sotBoolean.${name}.${rid}`, setBoolean(name, value));
+ipcRenderer.on('Settings.setBoolean', (event, rid: string, name: string, value: boolean, session: boolean = false) => {
+	ipcRenderer.send(`Settings.sotBoolean.${name}.${rid}`, setBoolean(name, value, session));
 });
-ipcRenderer.on('Settings.getString', (event, rid: string, name: string, defaultValue: string) => {
-	ipcRenderer.send(`Settings.gotString.${name}.${rid}`, getString(name, defaultValue));
+ipcRenderer.on('Settings.getString', (event, rid: string, name: string, defaultValue: string, session: boolean = false) => {
+	ipcRenderer.send(`Settings.gotString.${name}.${rid}`, getString(name, defaultValue, session));
 });
-ipcRenderer.on('Settings.setString', (event, rid: string, name: string, value: string) => {
-	ipcRenderer.send(`Settings.sotString.${name}.${rid}`, setString(name, value));
+ipcRenderer.on('Settings.setString', (event, rid: string, name: string, value: string, session: boolean = false) => {
+	ipcRenderer.send(`Settings.sotString.${name}.${rid}`, setString(name, value, session));
 });
-ipcRenderer.on('Settings.getJSON', (event, rid: string, name: string, defaultValue: Object) => {
-	ipcRenderer.send(`Settings.gotJSON.${name}.${rid}`, getJSON(name, defaultValue));
+ipcRenderer.on('Settings.getJSON', (event, rid: string, name: string, defaultValue: Object, session: boolean = false) => {
+	ipcRenderer.send(`Settings.gotJSON.${name}.${rid}`, getJSON(name, defaultValue, session));
 });
-ipcRenderer.on('Settings.setJSON', (event, rid: string, name: string, value: Object) => {
-	ipcRenderer.send(`Settings.sotJSON.${name}.${rid}`, setJSON(name, value));
+ipcRenderer.on('Settings.setJSON', (event, rid: string, name: string, value: Object, session: boolean = false) => {
+	ipcRenderer.send(`Settings.sotJSON.${name}.${rid}`, setJSON(name, value, session));
 });
 
 export function language(lang?: string) {
@@ -40,11 +40,13 @@ export function language(lang?: string) {
  * 
  * @param {String} name Name of the localStorage value
  * @param {Boolean} defaultValue The default value you want returned if storage value was not found
+ * @param {Boolean} [session=false] use sessionStorage instead
  * @returns {Boolean}
  */ 
-export function getBoolean(name: string, defaultValue: boolean): boolean {
+export function getBoolean(name: string, defaultValue: boolean, session: boolean = false): boolean {
+	if(typeof(session) !== 'boolean') session = false;
 	if(name.length <= 0) return defaultValue;
-	let item = window.localStorage.getItem(name);
+	let item = window[session ? 'sessionStorage' : 'localStorage'].getItem(name);
 	if(item != null) {
 		if(item == 'true') {
 			return true;
@@ -60,9 +62,11 @@ export function getBoolean(name: string, defaultValue: boolean): boolean {
  * 
  * @param {String} name Name of the localStorage value
  * @param {Boolean} value The value you want to set
+ * @param {Boolean} [session=false] use sessionStorage instead
  */
-export function setBoolean(name: string, value: boolean) {
-	window.localStorage.setItem(name, (value ? 'true' : 'false'));
+export function setBoolean(name: string, value: boolean, session: boolean = false) {
+	if(typeof(session) !== 'boolean') session = false;
+	window[session ? 'sessionStorage' : 'localStorage'].setItem(name, (value ? 'true' : 'false'));
 }
 
 /**
@@ -70,11 +74,13 @@ export function setBoolean(name: string, value: boolean) {
  * 
  * @param {String} name Name of the localStorage value
  * @param {String} defaultValue The default value you want returned if storage value was not found
+ * @param {Boolean} [session=false] use sessionStorage instead
  * @returns {String}
  */
-export function getString(name: string, defaultValue: string): string {
+export function getString(name: string, defaultValue: string, session: boolean = false): string {
+	if(typeof(session) !== 'boolean') session = false;
 	if(name.length <= 0) return defaultValue;
-	let item = window.localStorage.getItem(name);
+	let item = window[session ? 'sessionStorage' : 'localStorage'].getItem(name);
 	if(item != null) {
 		return item;
 	}
@@ -87,9 +93,11 @@ export function getString(name: string, defaultValue: string): string {
  * 
  * @param {String} name Name of the localStorage value
  * @param {String} value The value you want to set
+ * @param {Boolean} [session=false] use sessionStorage instead
  */
-export function setString(name: string, value: string) {
-	window.localStorage.setItem(name, value);
+export function setString(name: string, value: string, session: boolean = false) {
+	if(typeof(session) !== 'boolean') session = false;
+	window[session ? 'sessionStorage' : 'localStorage'].setItem(name, value);
 }
 
 /**
@@ -97,11 +105,13 @@ export function setString(name: string, value: string) {
  * 
  * @param {String} name Name of the localStorage value
  * @param {any} defaultValue The default value you want returned if storage value was not found
+ * @param {Boolean} [session=false] use sessionStorage instead
  * @returns {any}
  */
-export function getJSON(name: string, defaultValue: any): any {
+export function getJSON(name: string, defaultValue: any, session: boolean = false): any {
+	if(typeof(session) !== 'boolean') session = false;
 	if(name.length <= 0) return defaultValue;
-	let item = window.localStorage.getItem(name);
+	let item = window[session ? 'sessionStorage' : 'localStorage'].getItem(name);
 	if(item != null) {
 		try {
 			return JSON.parse(item);
@@ -115,10 +125,12 @@ export function getJSON(name: string, defaultValue: any): any {
  * 
  * @param {String} name Name of the localStorage value
  * @param {Object} value The value you want to set
+ * @param {Boolean} [session=false] use sessionStorage instead
  */
-export function setJSON(name: string, value: Object) {
+export function setJSON(name: string, value: Object, session: boolean = false) {
+	if(typeof(session) !== 'boolean') session = false;
 	try {
-		window.localStorage.setItem(name, JSON.stringify(value));
+		window[session ? 'sessionStorage' : 'localStorage'].setItem(name, JSON.stringify(value));
 	} catch(e) { console.error(e); }
 }
 
@@ -126,7 +138,9 @@ export function setJSON(name: string, value: Object) {
  * Removes an item in the localStorage
  * 
  * @param {String} name 
+ * @param {Boolean} [session=false] use sessionStorage instead
  */
-export function remove(name: string) {
-	window.localStorage.removeItem(name);
+export function remove(name: string, session: boolean = false) {
+	if(typeof(session) !== 'boolean') session = false;
+	window[session ? 'sessionStorage' : 'localStorage'].removeItem(name);
 }
